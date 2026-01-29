f414ac14bb3106ac05ee58f2146fafb9c1dd28a1
iahub360-stack <iahub360@gmail.com>
2026-01-29 09:24:20 -0300

    Debug Divers

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI, Request
-from .webhooks.mistic import handle_misticpay_webhook
+from webhooks.mistic import handle_misticpay_webhook
 
 app = FastAPI()
 
 @app.post("/webhooks/misticpay")
 async def misticpay_webhook(request: Request):
-    # bot ainda None nesta fase (ok)
     return await handle_misticpay_webhook(request, bot=None)
-
